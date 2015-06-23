a280c30e52266994e69899447f10eb5924489163
Marc Boanas <marcjohnboanas@gmail.com>
2015-06-23 14:39:06 +0100

    first commit

---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: 'mongodb://localhost/mean-book',
+  sessionSecret: 'developmentSessionSecret',
+  facebook: {
+    clientID: '364645040391124',
+    clientSecret: '75971f4a11e5f3c9e2b9918c8b063754',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  }
+};
