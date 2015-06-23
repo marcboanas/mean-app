336847e45a2584f32dc2e6553cc4d8f197da5ffa
Marc Boanas <marcjohnboanas@gmail.com>
2015-06-23 14:51:12 +0100

    config/env/production.js

---
+++ config/env/production.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  db: 'mongodb://marcboanas:S25rhsebt09@ds063859.mongolab.com:63859/mean-app-db',
+  sessionSecret: 'developmentSessionSecret',
+  facebook: {
+    clientID: '364645040391124',
+    clientSecret: '75971f4a11e5f3c9e2b9918c8b063754',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  }
+};
